e014263e72bcca209bb100ee284bce081de3aa7a
LuisAngeloJota <c82w3b2hu@outlook.com>
2024-06-19 09:25:52 +0800

    Windows11Tweaks.ps1: Update

--- Windows11Tweaks.ps1
+++ Windows11Tweaks.ps1
@@ -12,13 +12,11 @@ Disable-MMAgent -mc
 # Disable Firewall
 New-Item -Path "HKLM:\Software\Policies\Microsoft\WindowsFirewall\DomainProfile" -ItemType Directory -Force
 New-Item -Path "HKLM:\Software\Policies\Microsoft\WindowsFirewall\StandardProfile" -ItemType Directory -Force
-
 Set-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\WindowsFirewall\DomainProfile" -Name "EnableFirewall" -Value 0 -Type Dword -Force
 Set-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\WindowsFirewall\StandardProfile" -Name "EnableFirewall" -Value 0 -Type Dword -Force
 
 # Set PageFile
 Set-ItemProperty -Path "HKLM:\System\CurrentControlSet\Control\Session Manager\Memory Management" -Name "PagingFiles" -Value "C:\pagefile.sys 8192 8192" -Type String -Force
-
 
 # Disable Mitigations
 Set-ItemProperty -Path "HKLM:\System\CurrentControlSet\Control\Session Manager\Memory Management" -Name "FeatureSettings" -Value 1 -Type DWord -Force
@@ -33,7 +31,6 @@ New-Item -Path "HKLM:\Software\Policies\Microsoft\Windows\WindowsUpdate" -ItemTy
 New-Item -Path "HKLM:\Software\Policies\Microsoft\Windows\DeliveryOptimization" -ItemType Directory -Force
 New-Item -Path "HKLM:\Software\Policies\Microsoft\Windows\WindowsUpdate\AU" -ItemType Directory -Force
 New-Item -Path "HKLM:\Software\Policies\Microsoft\Windows\DriverSearching" -ItemType Directory -Force
-
 Set-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Windows\WindowsUpdate" -Name "ExcludeWUDriversInQualityUpdate" -Value 1 -Type Dword -Force
 Set-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Windows\DeliveryOptimization" -Name "DODownloadMode" -Value 99 -Type Dword -Force
 Set-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Windows\WindowsUpdate\AU" -Name "NoAutoUpdate" -Value 1 -Type Dword -Force
@@ -47,6 +44,9 @@ Set-ItemProperty -Path "HKLM:\System\CurrentControlSet\Control\FileSystem" -Name
 
 # Enable Powershell RemoteSigned Scripts
 New-Item -Path "HKLM:\Software\Policies\Microsoft\Windows\PowerShell" -ItemType Directory -Force
-
 Set-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Windows\PowerShell" -Name "EnableScripts" -Value 1 -Type Dword -Force
 Set-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Windows\PowerShell" -Name "ExecutionPolicy" -Value "RemoteSigned" -Type String -Force
+
+# Disable Automatic Scheduled Maintenance
+New-Item -Path "HKLM:\Software\Policies\Microsoft\Windows\ScheduledDiagnostics" -ItemType Directory -Force
+Set-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Windows\ScheduledDiagnostics" -Name "EnabledExecution" -Value 0 -Type Dword -Force
